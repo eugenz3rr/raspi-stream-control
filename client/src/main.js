2d5d01fc033793529384befba211f39ede9f8434
Your Name <eugen.zerr@hotmail.com>
2019-12-08 21:37:56 +0100

    Added code.

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,7 @@ import Vuetify from "vuetify/dist/vuetify.min";
 import VueSocketIOExt from 'vue-socket.io-extended';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8081');
+const socket = io('http://stream:8081');
 
 Vue.use(Vuetify);
 Vue.use(VueSocketIOExt, socket);

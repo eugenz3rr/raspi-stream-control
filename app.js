8a9e3c35f47f980145b31953c5e0271ecff36d47
Your Name <eugen.zerr@hotmail.com>
2019-12-08 21:34:11 +0100

    Added commented code.

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 const http = require('http');
 const express = require('express');
-//const WebStreamerServer = require('./server/lib/raspivid');
+const WebStreamerServer = require('./server/lib/raspivid');
 const app  = express();
 app.use(express.static(__dirname + '/client'));
 
 const server  = http.createServer(app);
-//const silence = new WebStreamerServer(server);
+const silence = new WebStreamerServer(server);
 
 const io = require('socket.io')(server);
 
